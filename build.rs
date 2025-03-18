8fe08cd79d904c8f71f8d5601445b3d84a6cb738
pythcoiner <pythcoiner@proton.me>
2025-03-18 19:47:48 +0100

    update build script to automatically rerun if a *.rs file is edited

--- build.rs
+++ build.rs
@@ -1,9 +1,17 @@
+use std::fs;
+
 use cxx_qt_build::CxxQtBuilder;
 
 fn main() {
     CxxQtBuilder::new()
         .file("src/lib.rs")
-        // .file("src/coin.rs")
-        // .file("src/pool_config.rs")
         .build();
+
+    // Manually list all .rs files under src/
+    for entry in fs::read_dir("src").unwrap() {
+        let path = entry.unwrap().path();
+        if path.extension().and_then(|e| e.to_str()) == Some("rs") {
+            println!("cargo::rerun-if-changed={}", path.display());
+        }
+    }
 }
